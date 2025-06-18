1d35be1efb773a168a75f3ce2a98e04bf7b97951
Ann <kanjanawan.ph@gmail.com>
2025-06-18 22:40:37 +0700

    fix thread id

--- src/store/chatStore.ts
+++ src/store/chatStore.ts
@@ -1,15 +1,20 @@
 import { create } from "zustand";
 import type { Message } from "../types";
+import { v4 as uuidv4 } from "uuid";
 
 type ChatState = {
-  messageList: Message[]
+  messageList: Message[];
   setMessageList: (message: Message) => void;
   clearMessageList: () => void;
+  threadId: string;
+  setThreadId: () => void;
 };
 
 export const useChatStore = create<ChatState>((set) => ({
   messageList: [],
   setMessageList: (message: Message) =>
     set((state) => ({ messageList: [...state.messageList, message] })),
-  clearMessageList: () => set({ messageList: [] })
+  clearMessageList: () => set({ messageList: [] }),
+  threadId: uuidv4(),
+  setThreadId: () => set({ threadId: uuidv4() }),
 }));
